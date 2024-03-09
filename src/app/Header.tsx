de073fb9c52e5a51fa216c778de3a3f4106a9318
Saumitra Pandey <saumitra.1997.fcb@gmail.com>
2024-03-09 17:09:23 +0530

    removed invalid char

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <>
         <h1 className="mb-1 px-3 py-10 sm:px-20 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">Enter a name to predict <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-800 from-sky-600">Age, Gender and Countries</span></h1>
-        <p className="mb-1 px-2 py-10 text-lg font-normal text-gray-500 lg:text-xl sm:px-32 xl:px-48 dark:text-gray-400 text-center">Searching names will give result for a person's age, gender and list of all countries with probability(click on three dots ... to reveal all countries)</p>
+        <p className="mb-1 px-2 py-10 text-lg font-normal text-gray-500 lg:text-xl sm:px-32 xl:px-48 dark:text-gray-400 text-center">Searching names will give result for a person&apos;s age, gender and list of all countries with probability(click on three dots ... to reveal all countries)</p>
     </>
   )
 }
